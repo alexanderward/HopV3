7a11b85983458a1e0a93cd477daad478da8b8d56
Alexander Ward <alexanderward@gmail.com>
2021-03-15 00:37:20 -0500

    working lambda deployment

--- services/terraform/dev/pip.sh
+++ services/terraform/dev/pip.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
-
+rm -rf ../../terraform/source
+mkdir ../../terraform/source
 pip3 install -r ../../requirements/dev.txt -t $1 --upgrade
-rsync -av ../.. ../../terraform/source --exclude ../../terraform --exclude ../../requirements --exclude ../../node_modules
+rsync -arv --rsync-path=../../ --exclude=node_modules --exclude=terraform --exclude=requirements ../../ $1
